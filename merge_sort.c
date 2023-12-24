01f1d372c3d8f4f0666955beabed3ab18d1d372c
DentistOmarAshraf <omar_ashraf@live.com>
2023-12-24 07:10:09 +0000

    update

--- merge_sort.c
+++ merge_sort.c
@@ -1,61 +1,65 @@
 #include "main.h"
-/*-----------header----------*/
-void merge_sort(int arr[], int len);
-void merge_rec(int arr[], int l, int r);
-void merge_sorted(int arr[], int l, int m, int r);
-/*----------function---------*/
+/*--------Merge Header--------*/
 
-void merge_sort(int arr[], int len)
+void merge(int *, int);
+void merge_rec(int *, int , int);
+void merge_sort(int*, int, int, int);
+
+/*----------Merge sort Function------*/
+
+void merge(int arr[], int len)
 {
 	merge_rec(arr, 0, len - 1);
 }
+
 void merge_rec(int arr[], int l, int r)
 {
-	int mid = l + (r - l) / 2;
-	int i;
-
+	int m;
+	
 	if (l < r)
 	{
-		merge_rec(arr, l, mid);
-		merge_rec(arr, mid + 1, r);
-		merge_sorted(arr, l, mid, r);
+		m = l + (r - l) / 2;
+		merge_rec(arr, l, m);
+		merge_rec(arr, m + 1, r);
+		merge_sort(arr, l, m, r);
 	}
 }
-void merge_sorted(int arr[], int l, int m, int r)
+
+void merge_sort(int arr[], int l, int m, int r)
 {
+	int left_size = m - l + 1;
+	int right_size = r - m;
+	int temp_l[left_size], temp_r[right_size];
 	int i, j, k;
-	int left_length = m - l + 1;
-	int right_length = r - m;
-	int left_arr[left_length], right_arr[right_length];
 
-	for (i = 0 ; i < left_length ; i++)
-		left_arr[i] = arr[l + i];
+	for (i = 0 ; i < left_size ; i++)
+		temp_l[i] = arr[i + l];
 
-	for (i = 0 ; i < right_length ; i++)
-		right_arr[i] = arr[m + 1 + i];
+	for (i = 0 ; i < right_size ; i++)
+		temp_r[i] = arr[m + i + 1];
 
-	for (i = 0, j = 0, k = l; k <= r; k++)
+	for (i = 0, j = 0, k = l ; k <= r ; k++)
 	{
-		if ((i < left_length) 
-				&& (j >= right_length || (left_arr[i] <= right_arr[j])))
+		if ((i < left_size) && ((j >= right_size) || (temp_l[i] < temp_r[j])))
 		{
-			arr[k] = left_arr[i];
+			arr[k] = temp_l[i];
 			i++;
 		}
 		else
 		{
-			arr[k] = right_arr[j];
+			arr[k] = temp_r[j];
 			j++;
 		}
 	}
-}
 
+}
+/*----------------MAIN------------------*/
 int main(void)
 {
 	int arr[] = {9, 2, 3, 1, 0, 7, 6, 5, 4, 8};
 	int len = sizeof(arr)/ sizeof(arr[0]);
 
-	merge_sort(arr, len);
+	merge(arr, len);
 	print_list(arr, len);
 
 	return (0);
